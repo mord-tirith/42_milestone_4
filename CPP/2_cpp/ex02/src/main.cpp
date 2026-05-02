0cf43be78b045ebffcb511e7ad555e532a7bfaba
mord-tirith <thiagohfreitasn@gmail.com>
2026-05-02 22:52:29 +0100

    not so bad, really

---
+++ CPP/2_cpp/ex02/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thenriqu <thenriqu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 19:47:35 by thenriqu          #+#    #+#             */
+/*   Updated: 2026/05/02 22:12:53 by mord             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <ostream>
+
+int	main(void) {
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+
+	return 0;
+}
