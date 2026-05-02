a950fd7a0b2506cd18d2da948d757719b4ab2aab
mord-tirith <thiagohfreitasn@gmail.com>
2026-05-02 12:30:23 +0100

    structured ex04 to Makefile system

--- CPP/1_cpp/ex04/src/main.cpp
+++ CPP/1_cpp/ex04/src/main.cpp
@@ -10,25 +10,25 @@ int	errorOut(std::string msg) {
 
 int	main(int ac, char **av) {
 	if (ac != 4)
-		errorOut("you need to provide a file, a target word and a replacement word!");
+		return errorOut("you need to provide a file, a target word and a replacement word!");
 
 	std::string	filename = av[1];
 	std::string	target = av[2];
 	std::string	replacement = av[3];
 
 	if (filename.empty() || target.empty())
-		errorOut("filename and Target can't be empty strings!");
+		return errorOut("filename and Target can't be empty strings!");
 
-	std::ifstream	inFile(filename.c_str());
-	std::ofstream	outFile((filename + ".replace").c_str());
 	std::string		line;
 	size_t			targetSize = target.length();
 
+	std::ifstream	inFile(filename.c_str());
 	if (!inFile)
-		errorOut("could not open input file.");
+		return errorOut("could not open input file.");
 
+	std::ofstream	outFile((filename + ".replace").c_str());
 	if (!outFile)
-		errorOut("could not create output file.");
+		return errorOut("could not create output file.");
 
 	while (std::getline(inFile, line))
 	{
@@ -47,4 +47,5 @@ int	main(int ac, char **av) {
 		if (!inFile.eof())
 			outFile << '\n';
 	}
+	return 0;
 }
